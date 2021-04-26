c2338a667d61df56f35e63d9c7c6d145cd3b402c
shuiyihang <shuiyihang0981@163.com>
2021-04-26 17:58:23 +0800

    修改：似乎又说不清楚的问题

--- showUI.py
+++ showUI.py
@@ -51,10 +51,22 @@ def hit_wall_detect(instance):
             instance.y=instance.r
         instance.dir_y=-instance.dir_y
 def coll_speed(body1,body2):
-    body1.speed_x = ((body1.r-body2.r)*body1.speed_x + 2*body2.r*body2.speed_x)/(body1.r+body2.r)
-    body1.speed_y = ((body1.r-body2.r)*body1.speed_y + 2*body2.r*body2.speed_y)/(body1.r+body2.r)
-    body2.speed_x = ((body2.r-body1.r)*body2.speed_x + 2*body1.r*body1.speed_x)/(body1.r+body2.r)
-    body2.speed_y = ((body2.r-body1.r)*body2.speed_y + 2*body1.r*body1.speed_y)/(body1.r+body2.r)
+    body1.speed_x = ((body1.r-body2.r)*body1.speed_x*body1.dir_x + 2*body2.r*body2.speed_x*body2.dir_x)//(body1.r+body2.r)
+    if body1.speed_x<0:
+        body1.dir_x = -body1.dir_x
+        body1.speed_x = -body1.speed_x
+    body1.speed_y = ((body1.r-body2.r)*body1.speed_y*body1.dir_y + 2*body2.r*body2.speed_y*body2.dir_y)//(body1.r+body2.r)
+    if body1.speed_y<0:
+        body1.dir_y = -body1.dir_y
+        body1.speed_y = -body1.speed_y
+    body2.speed_x = ((body2.r-body1.r)*body2.speed_x*body2.dir_x + 2*body1.r*body1.speed_x*body1.dir_x)//(body1.r+body2.r)
+    if body2.speed_x<0:
+        body2.dir_x = -body2.dir_x
+        body2.speed_x = -body2.speed_x
+    body2.speed_y = ((body2.r-body1.r)*body2.speed_y*body2.dir_y + 2*body1.r*body1.speed_y*body1.dir_y)//(body1.r+body2.r)
+    if body2.speed_y<0:
+        body2.dir_y = -body2.dir_y
+        body2.speed_y = -body2.speed_y
 
 def hit_body_detect(body1,body2):
 
@@ -63,169 +75,11 @@ def hit_body_detect(body1,body2):
     if (delta_x**2+delta_y**2)<(body1.r+body2.r+80)**2:
         draw_line(body1.x,body1.y,body2.x,body2.y,"red")
     if (delta_x**2+delta_y**2)<=((body1.r+body2.r)**2):#碰撞了
-        
-        # 怎么做能让他不穿透呢???
-        if body1.dir_x !=body2.dir_x:
-            if body1.dir_y != body2.dir_y:#x,y方向都不相同
-
-                body1.dir_x=-body1.dir_x
-                body1.dir_y=-body1.dir_y
-                body2.dir_x=-body2.dir_x
-                body2.dir_y=-body2.dir_y
-            else:#  x方向不同，y方向相同
-                if body1.dir_y>0:#此时都是向下走的
-                    if body1.y<body2.y:#说明是body1撞了body2,那么对body1做方向取反
-                        #需要对被撞球做一个速度合成，改变x方向，改变x,y速度
-                        if body2.speed_x<body1.speed_x:
-                            body2.dir_x=-body2.dir_x#方向取反
-                        #     body2.speed_x=body1.speed_x-body2.speed_x
-                        # else:
-                        #     body2.speed_x-=body1.speed_x
-
-                        #碰撞之后没速度了
-                        # if body2.speed_x==0:
-                        #     body2.speed_x=1
-                        # body2.speed_y=body1.speed_y#######被撞球的速度分量获取撞他的速度
-
-                        body1.dir_x=-body1.dir_x
-                        body1.dir_y=-body1.dir_y
-                    else:#body2撞了test_spirt
-                        if body1.speed_x<body2.speed_x:
-                            body1.dir_x=-body1.dir_x#方向取反
-                        #     body1.speed_x=body2.speed_x-body1.speed_x
-                        # else:
-                        #     body1.speed_x-=body2.speed_x
-                        #碰撞之后没速度了
-                        # if body1.speed_x==0:
-                        #     body1.speed_x=1
-                        # body1.speed_y=body2.speed_y
-
-                        body2.dir_x=-body2.dir_x
-                        body2.dir_y=-body2.dir_y
-                else:#都是向上走的
-                    if body1.y<body2.y:#body2撞了test
-                        if body1.speed_x<body2.speed_x:#判断一下x方向的分量
-                            body1.dir_x=-body1.dir_x
-                        #     body1.speed_x=body2.speed_x-body1.speed_x
-                        # else:
-                        #     body1.speed_x-=body2.speed_x
-                        #碰撞之后没速度了
-                        # if body1.speed_x==0:
-                        #     body1.speed_x=1
-                        # body1.speed_y=body2.speed_y
-
-                        body2.dir_x=-body2.dir_x
-                        body2.dir_y=-body2.dir_y
-                    else:
-                        if body2.speed_x<body1.speed_x:
-                            body2.dir_x=-body2.dir_x#方向取反
-                        #     body2.speed_x=body1.speed_x-body2.speed_x
-                        # else:
-                        #     body2.speed_x-=body1.speed_x
-                        #碰撞之后没速度了
-                        # if body2.speed_x==0:
-                        #     body2.speed_x=1
-                        # body2.speed_y=body1.speed_y#######被撞球的速度分量获取撞他的速度
-
-                        body1.dir_x=-body1.dir_x
-                        body1.dir_y=-body1.dir_y
-
-
-        elif body1.dir_y != body2.dir_y:#x方向相同，y方向不同
-
-            if body1.dir_x>0:#都是向右走
-                if body1.x<body2.x:#说明是body1撞了body2,那么对body1做方向取反
-                    #需要对被撞球做一个速度合成，改变x方向，改变x,y速度
-                    if body2.speed_y<body1.speed_y:
-                        body2.dir_y=-body2.dir_y#方向取反
-                    #     body2.speed_y=body1.speed_y-body2.speed_y
-                    # else:
-                    #     body2.speed_y-=body1.speed_y
-                    
-                    #碰撞之后没速度了
-                    # if body2.speed_y==0:
-                    #     body2.speed_y=1
-                    # body2.speed_x=body1.speed_x#######被撞球的速度分量获取撞他的速度
-
-                    body1.dir_x=-body1.dir_x
-                    body1.dir_y=-body1.dir_y
-                else:#body2撞了test_spirt
-                    if body1.speed_y<body2.speed_y:#判断一下x方向的分量
-                        body1.dir_y=-body1.dir_y
-                    #     body1.speed_y=body2.speed_y-body1.speed_y
-                    # else:
-                    #     body1.speed_y-=body2.speed_y
-                    #碰撞之后没速度了
-                    # if body1.speed_y==0:
-                    #     body1.speed_y=1
-                    # body1.speed_x=body2.speed_x
-
-                    body2.dir_x=-body2.dir_x
-                    body2.dir_y=-body2.dir_y
-            else:#都是向左走
-                if body1.x<body2.x:#body2撞了test
-                    if body1.speed_y<body2.speed_y:#判断一下x方向的分量
-                        body1.dir_y=-body1.dir_y
-                    #     body1.speed_y=body2.speed_y-body1.speed_y
-                    # else:
-                    #     body1.speed_y-=body2.speed_y
-                    #碰撞之后没速度了
-                    # if body1.speed_y==0:
-                    #     body1.speed_y=1
-                    # body1.speed_x=body2.speed_x
-
-                    body2.dir_x=-body2.dir_x
-                    body2.dir_y=-body2.dir_y
-                else:#test撞了body2
-                    if body2.speed_y<body1.speed_y:
-                        body2.dir_y=-body2.dir_y#方向取反
-                    #     body2.speed_y=body1.speed_y-body2.speed_y
-                    # else:
-                    #     body2.speed_y-=body1.speed_y
-                    #碰撞之后没速度了
-                    # if body2.speed_y==0:
-                    #     body2.speed_y=1
-                    # body2.speed_x=body1.speed_x#######被撞球的速度分量获取撞他的速度
-
-                    body1.dir_x=-body1.dir_x
-                    body1.dir_y=-body1.dir_y
-
-
-        else:#方向都是相同的
-            if body1.dir_y>0:
-                if body1.y>body2.y:#body2撞了test
-                    body2.dir_x=-body2.dir_x
-                    body2.dir_y=-body2.dir_y
-                    # if body1.speed_x<body2.speed_x:
-                    #     body1.speed_x=body2.speed_x
-                    # if body1.speed_y<body2.speed_y:
-                    #     body1.speed_y=body2.speed_y
-                else:
-                    body1.dir_x=-body1.dir_x
-                    body1.dir_y=-body1.dir_y
-
-                    # if body2.speed_x<body1.speed_x:
-                    #     body2.speed_x=body1.speed_x
-                    # if body2.speed_y<body1.speed_y:
-                    #     body2.speed_y=body1.speed_y
-            else:
-                if body1.y>body2.y:#test撞了body2
-                    body1.dir_x=-body1.dir_x
-                    body1.dir_y=-body1.dir_y
-
-                    # if body2.speed_x<body1.speed_x:
-                    #     body2.speed_x=body1.speed_x
-                    # if body2.speed_y<body1.speed_y:
-                    #     body2.speed_y=body1.speed_y
-
-                else:#body2撞了test
-                    body2.dir_x=-body2.dir_x
-                    body2.dir_y=-body2.dir_y
-                    # if body1.speed_x<body2.speed_x:
-                    #     body1.speed_x=body2.speed_x
-                    # if body1.speed_y<body2.speed_y:
-                    #     body1.speed_y=body2.speed_y
         coll_speed(body1,body2)
+        
+        
+        
+        
 
 def rand_init_speed(ins_zero):
     ins_zero.speed_x=random.randint(1,5)
@@ -277,7 +131,7 @@ if __name__=="__main__":
         
         
 
-        if cnt>10:
+        if cnt>20:
             for i in range(len(spirit_list)):
                 for j in range(i+1,len(spirit_list)):
                     hit_body_detect(spirit_list[i],spirit_list[j])
@@ -286,30 +140,6 @@ if __name__=="__main__":
         time.sleep(0.02)
     
 
-    
-
-    # for i in range(6):
-    #     _new_k=random.randint(0,90)
-    #     _new_r=random.randint(0,20)
-    #     _new_dir=random.randint(0,2)
-    #     circle_buff.put(spirit(k=_new_k,r=_new_r,dir=_new_dir))
-
-    # while not circle_buff.empty():
-
-    #     temp_spirit=circle_buff.get()
-
-        
-    #     if temp_spirit.dir==0:
-    #         spirit.x+=1
-    #     else:
-    #         spirit-=1
-    #     if sp
-
-
-
-
-
-    #     if spirit.x>720 or spirit<0:
 
 
     
